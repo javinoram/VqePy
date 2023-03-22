5206e03750d85a17d046dfbd882c112c4130b072
jnorambu <javinorambl@gmail.com>
2023-03-22 14:49:30 -0300

    effect of the magnetic field added

--- VQEclass.py
+++ VQEclass.py
@@ -1,5 +1,5 @@
 from Ansatzs import *
-import scipy as sc
+from PhysicsFormulas import *
 
 class hamiltonian():
     #Class variables
@@ -19,7 +19,7 @@ class hamiltonian():
     #Cicuit execution
     ansatz_circuit: QuantumCircuit = None
     backend = None
-    shots = 2**12
+    shots = 2**15
     
     #Optimization function params
     optimization_method = "" 
@@ -50,70 +50,85 @@ class hamiltonian():
     Optimization functions
     '''
     def ground_state_calculation(self, theta: list, h: float) -> float:
-        general_cost_function = lambda theta: self.cost_function_VQE(theta) + self.cost_function_magnetic_field(theta, h)
+        general_cost_function = lambda theta: self.cost_function_VQE(theta, h)
         xs = sc.optimize.minimize(general_cost_function, theta, method=self.optimization_method,
                                   options=self.optimization_alg_params, )['x']
-        self.hamiltonian_eigenvalues.append(self.cost_function_VQE(xs))
-        self.hamiltonian_eigenvectors.append(xs)
-        return self.hamiltonian_eigenvalues[0]
+        #self.hamiltonian_eigenvalues.append(self.cost_function_VQE(xs, h))
+        #self.hamiltonian_eigenvectors.append(xs)
+        #return self.hamiltonian_eigenvalues[0]
+        return xs, self.cost_function_VQE(xs, h)
     
-    def excited_state_calculation(self, theta: list, number_level: int, h: float) -> float:
-        general_cost_function = lambda theta: self.cost_function_VQE(theta) + self.cost_function_magnetic_field(theta, h)
-        for _ in range(number_level):
-            xs = sc.optimize.minimize(general_cost_function, theta, method=self.optimization_method,
-                                    options=self.optimization_alg_params, )['x']
-            self.hamiltonian_eigenvalues.append(self.cost_function_VQE(xs))
-            self.hamiltonian_eigenvectors.append(xs)
-        return self.hamiltonian_eigenvalues
+    #def excited_state_calculation(self, theta: list, number_level: int, h: float) -> float:
+    #    general_cost_function = lambda theta: self.cost_function_VQE(theta) + self.cost_function_magnetic_field(theta, h)
+    #    for _ in range(number_level):
+    #        xs = sc.optimize.minimize(general_cost_function, theta, method=self.optimization_method,
+    #                                options=self.optimization_alg_params, )['x']
+    #        self.hamiltonian_eigenvalues.append(self.cost_function_VQE(xs))
+    #        self.hamiltonian_eigenvectors.append(xs)
+    #    return self.hamiltonian_eigenvalues
 
     '''
     Cost functions VQE, VQD ...
     '''
-    def cost_function_VQE(self, theta: list) -> float:
+    def cost_function_VQE(self, theta: list, h: float) -> float:
         func_val = 0.0
+
+        circuit = hardware_efficient_ansatz(theta, self.number_qubits, 2, self.number_ansatz_repetition)
         for i, hamiltonian_term in enumerate(self.hamiltonian_list_index):
-            circuit = hardware_efficient_ansatz(theta, self.number_qubits, 2, self.number_ansatz_repetition)
+            circuit_copy = circuit
             for j, component in enumerate(hamiltonian_term):
                 if 'X' == component[0]:
-                    circuit.h( component[1] )
+                    circuit_copy.h( component[1] )
                 
                 if 'Y' == component[0]:
-                    circuit.sdg( component[1] )
-                    circuit.h( component[1] )
-                circuit.measure( [component[1]], [j])
+                    circuit_copy.sdg( component[1] )
+                    circuit_copy.h( component[1] )
+                circuit_copy.measure( [component[1]], [j])
 
-            job = execute( circuit, self.backend, shots=self.shots)
+            job = execute( circuit_copy, self.backend, shots=self.shots)
             result = job.result().get_counts()
             for data_row in result:
                 if data_row == '00' or data_row == '11':
                     func_val += self.hamiltonian_terms[i][1]*result[data_row]/self.shots
                 else:
                     func_val -= self.hamiltonian_terms[i][1]*result[data_row]/self.shots
-        return func_val
-    
-    def cost_function_VQD(self, theta: list) -> float:
-        func_val = self.cost_function_VQE( theta )
-        for previus_theta in self.hamiltonian_eigenvectors:
-
-            overlap_circuit = hardware_efficient_ansatz_overlap(theta, previus_theta, self.number_qubits, 2, self.number_ansatz_repetition)
-            overlap_circuit.measure([ i for i in range(self.number_qubits)], [ i for i in range(self.number_qubits)])
             
-            job = execute( overlap_circuit, self.backend, shots=self.shots)
-            result = job.result().get_counts()
-            if '000' in result:
-                func_val += 50*result['000']/self.shots
-        return func_val
-
-    def cost_function_magnetic_field(self, theta: list, h: float) -> float:
-        func_val = 0.0
+        circuit = hardware_efficient_ansatz(theta, self.number_qubits, 1, self.number_ansatz_repetition)
         for qubit_index in range(self.number_qubits):
-            circuit = hardware_efficient_ansatz(theta, self.number_qubits, 1, self.number_ansatz_repetition)
-            circuit.measure([qubit_index], [0])
-            
-            job = execute( circuit, self.backend, shots=self.shots)
+            circuit_copy = circuit
+            circuit_copy.measure([qubit_index], [0])
+                
+            job = execute( circuit_copy, self.backend, shots=self.shots)
             result = job.result().get_counts()
             if '0' in result:
-                func_val += -h*self.nu_b*self.gyromagnetic_factor*result['0']/self.shots
+                func_val += h*self.nu_b*self.gyromagnetic_factor*result['0']/self.shots
             else:
-                func_val -= -h*self.nu_b*self.gyromagnetic_factor*result['1']/self.shots
+                func_val -= h*self.nu_b*self.gyromagnetic_factor*result['1']/self.shots
         return func_val
+    
+    #def cost_function_VQD(self, theta: list) -> float:
+    #    func_val = self.cost_function_VQE( theta )
+    #    for previus_theta in self.hamiltonian_eigenvectors:
+
+    #        overlap_circuit = hardware_efficient_ansatz_overlap(theta, previus_theta, self.number_qubits, 2, self.number_ansatz_repetition)
+    #        overlap_circuit.measure([ i for i in range(self.number_qubits)], [ i for i in range(self.number_qubits)])
+            
+    #        job = execute( overlap_circuit, self.backend, shots=self.shots)
+    #        result = job.result().get_counts()
+    #        if '000' in result:
+    #            func_val += 50*result['000']/self.shots
+    #    return func_val
+
+    #def cost_function_magnetic_field(self, theta: list, h: float) -> float:
+    #    func_val = 0.0
+    #    for qubit_index in range(self.number_qubits):
+    #        circuit = hardware_efficient_ansatz(theta, self.number_qubits, 1, self.number_ansatz_repetition)
+    #        circuit.measure([qubit_index], [0])
+    #        
+    #        job = execute( circuit, self.backend, shots=self.shots)
+    #        result = job.result().get_counts()
+    #        if '0' in result:
+    #            func_val += -h*self.nu_b*self.gyromagnetic_factor*result['0']/self.shots
+    #        else:
+    #            func_val -= -h*self.nu_b*self.gyromagnetic_factor*result['1']/self.shots
+    #    return func_val
--- VQEtest.py
+++ VQEtest.py
@@ -2,9 +2,9 @@ from Ansatzs import *
 from VQEclass import *
 
 params = {
-    'hamiltonian_terms': [(('XXI', np.sqrt(2))), (('ZZI', np.sqrt(2)))],
+    'hamiltonian_terms': [(('XXI', -np.sqrt(2))), (('ZZI', -np.sqrt(2)))],
     'number_qubits': 3,
-    'number_ansatz_repetition': 3,
+    'number_ansatz_repetition': 4,
     'backend': Aer.get_backend('qasm_simulator'),
     'optimization_alg_params': {'tol': 1e-06, 'maxiter': 1e3},
     'gyromagnetic_factor' : 2.0,
@@ -13,9 +13,9 @@ params = {
 }
 
 simulation_object = hamiltonian(params)
-x = np.linspace(0,1,11)*1000
+x = np.linspace(0,1,3)*1000
 for i in x:
-    print(simulation_object.ground_state_calculation( [0.0 for i in range(9)], x ))
+    print(simulation_object.ground_state_calculation( [0.0 for _ in range(12)], -i ))
 #print(simulation_object.excited_state_calculation( [0.0 for i in range(15)], 4, 100.0 ))
 
 
@@ -25,8 +25,8 @@ Sy = np.array([[0,-1j],[1j,0]])
 Sz = np.array([[1,0],[0,-1]])
 
 
+H = -np.sqrt(2)*(np.kron(Sx, np.kron(Sx, Si)) + np.kron(Sz, np.kron(Sz, Si))) 
 for i in x:
-    H = np.sqrt(2)*(np.kron(Sx, np.kron(Sx, Si)) + np.kron(Sz, np.kron(Sz, Si))) 
-    H = H + 2.0*5.7883818066e-5*i*(np.kron(Sz, np.kron(Si, Si))+ np.kron(Si, np.kron(Sz, Si))+ np.kron(Si, np.kron(Si, Sz)))
+    H = H - 2.0*5.7883818066e-5*i*(np.kron(Sz, np.kron(Si, Si))+ np.kron(Si, np.kron(Sz, Si))+ np.kron(Si, np.kron(Si, Sz)))
     ee, vv = la.eigh( H )
     print(np.round(ee[:5],5))
