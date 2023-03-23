a49dc20c3416f8c78a77d8edd4753016cc8c0459
jnorambu <javinorambl@gmail.com>
2023-03-23 15:43:38 -0300

    first version of vqt

--- VQEclass.py
+++ VQEclass.py
@@ -19,7 +19,7 @@ class hamiltonian():
     #Cicuit execution
     ansatz_circuit: QuantumCircuit = None
     backend = None
-    shots = 2**15
+    shots = 2**10
     
     #Optimization function params
     optimization_method = "" 
@@ -58,14 +58,11 @@ class hamiltonian():
         #return self.hamiltonian_eigenvalues[0]
         return xs, self.cost_function_VQE(xs, h)
     
-    #def excited_state_calculation(self, theta: list, number_level: int, h: float) -> float:
-    #    general_cost_function = lambda theta: self.cost_function_VQE(theta) + self.cost_function_magnetic_field(theta, h)
-    #    for _ in range(number_level):
-    #        xs = sc.optimize.minimize(general_cost_function, theta, method=self.optimization_method,
-    #                                options=self.optimization_alg_params, )['x']
-    #        self.hamiltonian_eigenvalues.append(self.cost_function_VQE(xs))
-    #        self.hamiltonian_eigenvectors.append(xs)
-    #    return self.hamiltonian_eigenvalues
+    def thermal_state_calculation(self, theta: list) -> float:
+        general_cost_function = lambda theta: self.cost_function_VQT(theta)
+        xs = sc.optimize.minimize(general_cost_function, theta, method=self.optimization_method,
+                                  options=self.optimization_alg_params, )['x']
+        return xs, self.cost_function_VQT(theta)
 
     '''
     Cost functions VQE, VQD ...
@@ -73,7 +70,7 @@ class hamiltonian():
     def cost_function_VQE(self, theta: list, h: float) -> float:
         func_val = 0.0
 
-        circuit = hardware_efficient_ansatz(theta, self.number_qubits, 2, self.number_ansatz_repetition)
+        circuit = hardware_efficient_ansatz('0000', theta, self.number_qubits, 2, self.number_ansatz_repetition)
         for i, hamiltonian_term in enumerate(self.hamiltonian_list_index):
             circuit_copy = circuit
             for j, component in enumerate(hamiltonian_term):
@@ -93,7 +90,7 @@ class hamiltonian():
                 else:
                     func_val -= self.hamiltonian_terms[i][1]*result[data_row]/self.shots
             
-        circuit = hardware_efficient_ansatz(theta, self.number_qubits, 1, self.number_ansatz_repetition)
+        circuit = hardware_efficient_ansatz('0000', theta, self.number_qubits, 1, self.number_ansatz_repetition)
         for qubit_index in range(self.number_qubits):
             circuit_copy = circuit
             circuit_copy.measure([qubit_index], [0])
@@ -104,6 +101,43 @@ class hamiltonian():
                 func_val += h*self.nu_b*self.gyromagnetic_factor*result['0']/self.shots
             else:
                 func_val -= h*self.nu_b*self.gyromagnetic_factor*result['1']/self.shots
+        return func_val
+
+
+    def cost_function_VQT(self, theta: list) -> float:
+        func_val = 0.0
+
+        prob = theta[:self.number_qubits]
+        prob_circuit = prob_function_ansatz(prob, self.number_qubits, self.number_qubits)
+        job = execute( prob_circuit, self.backend, shots=self.shots)
+        result_prob = job.result().get_counts()
+
+        for state in result_prob:
+            local_probability = result_prob[state]/self.shots
+            func_val -= local_probability*np.log(local_probability)
+
+        for state in result_prob:
+            density = hardware_efficient_ansatz(state, theta[self.number_qubits:], self.number_qubits, 2, self.number_ansatz_repetition)
+            local_probability = result_prob[state]/self.shots
+            for i, hamiltonian_term in enumerate(self.hamiltonian_list_index):
+                circuit_copy = density
+                for j, component in enumerate(hamiltonian_term):
+                    if 'X' == component[0]:
+                        circuit_copy.h( component[1] )
+                    
+                    if 'Y' == component[0]:
+                        circuit_copy.sdg( component[1] )
+                        circuit_copy.h( component[1] )
+                    circuit_copy.measure( [component[1]], [j])
+
+                job = execute( circuit_copy, self.backend, shots=self.shots)
+                result = job.result().get_counts()
+                for data_row in result:
+                    if data_row == '00' or data_row == '11':
+                        func_val += self.hamiltonian_terms[i][1]*result[data_row]/self.shots
+                    else:
+                        func_val -= self.hamiltonian_terms[i][1]*result[data_row]/self.shots
+        print(func_val)
         return func_val
     
     #def cost_function_VQD(self, theta: list) -> float:
--- VQEtest.py
+++ VQEtest.py
@@ -4,7 +4,7 @@ from VQEclass import *
 params = {
     'hamiltonian_terms': [(('XXI', -np.sqrt(2))), (('ZZI', -np.sqrt(2)))],
     'number_qubits': 3,
-    'number_ansatz_repetition': 4,
+    'number_ansatz_repetition': 5,
     'backend': Aer.get_backend('qasm_simulator'),
     'optimization_alg_params': {'tol': 1e-06, 'maxiter': 1e3},
     'gyromagnetic_factor' : 2.0,
@@ -13,10 +13,14 @@ params = {
 }
 
 simulation_object = hamiltonian(params)
-x = np.linspace(0,1,3)*1000
-for i in x:
-    print(simulation_object.ground_state_calculation( [0.0 for _ in range(12)], -i ))
-#print(simulation_object.excited_state_calculation( [0.0 for i in range(15)], 4, 100.0 ))
+xs, val = simulation_object.thermal_state_calculation( [0.0 for _ in range(3*5 + 3)] )
+print(xs)
+print(val)
+
+prob_circuit = prob_function_ansatz(xs[:3], 3, 3)
+job = execute( prob_circuit, Aer.get_backend('qasm_simulator'), shots=2**10)
+result = job.result().get_counts()
+print(result)
 
 
 Si = np.array([[1,0],[0,1]]) 
@@ -26,7 +30,7 @@ Sz = np.array([[1,0],[0,-1]])
 
 
 H = -np.sqrt(2)*(np.kron(Sx, np.kron(Sx, Si)) + np.kron(Sz, np.kron(Sz, Si))) 
-for i in x:
-    H = H - 2.0*5.7883818066e-5*i*(np.kron(Sz, np.kron(Si, Si))+ np.kron(Si, np.kron(Sz, Si))+ np.kron(Si, np.kron(Si, Sz)))
-    ee, vv = la.eigh( H )
-    print(np.round(ee[:5],5))
+#for i in x:
+#    H = H - 2.0*5.7883818066e-5*i*(np.kron(Sz, np.kron(Si, Si))+ np.kron(Si, np.kron(Sz, Si))+ np.kron(Si, np.kron(Si, Sz)))
+ee, vv = la.eigh( H )
+print(np.round(ee[:5],5))
