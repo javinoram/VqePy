b2f04da5899f5096b6f632cd486ce74237fe8faf
Javier Norambuena Leiva <jnorambu@MacBook-Air-de-Javier.local>
2023-03-29 21:20:33 -0300

    Adding spin model

--- Ansatzs.py
+++ Ansatzs.py
@@ -1,66 +1,36 @@
-import numpy as np
-import scipy.linalg as la
-from qiskit import QuantumCircuit, transpile, Aer, IBMQ, BasicAer, execute
-import qiskit.quantum_info as qi
+#import numpy as np
+from pennylane import numpy as np
 
-def variational_hamiltonian_ansatz(xs, qubits_number, hamiltonian_terms, index_list, reps):
-    qc = QuantumCircuit( qubits_number, 2)
-    for j in range(reps):
-        for i, pauli_string in enumerate(hamiltonian_terms):
-            if 'Y' in pauli_string[0]:
-                qc.rx(np.pi/2, index_list[i][1:])
-                qc.cnot( index_list[i][1], index_list[i][2])
-                qc.rz(xs[i + len(hamiltonian_terms)*j], index_list[i][2])
-                qc.cnot( index_list[i][1], index_list[i][2] )
-                qc.rx(-np.pi/2, index_list[i][1:])
-            
-            if 'X' in pauli_string[0]:
-                qc.ry(np.pi/2, index_list[i][1:])
-                qc.cnot( index_list[i][1], index_list[i][2] )
-                qc.rz(xs[i + len(hamiltonian_terms)*j], index_list[i][2])
-                qc.cnot( index_list[i][1], index_list[i][2] )
-                qc.ry(-np.pi/2, index_list[i][1:])
-            
-            if 'Z' in pauli_string[0]:
-                qc.cnot( index_list[i][1], index_list[i][2] )
-                qc.rz(xs[i + len(hamiltonian_terms)*j], index_list[i][2])
-                qc.cnot( index_list[i][1], index_list[i][2] )
-    return qc   
+import math
+import itertools
+import pennylane as qml
 
-def hardware_efficient_ansatz(state, xs, qubits_number, bits_number, reps):
-    qc = QuantumCircuit( qubits_number, bits_number)
 
-    for i in range(len(state)):
-        if state[i] == '1':
-            qc.x(i)
+def prob_dist(params):
+    return np.vstack([sigmoid(params), 1 - sigmoid(params)]).T
 
-    if qubits_number ==1:
-        for j in range(reps):
-            qc.ry( xs[j], 0)
-    else:
-        for j in range(reps-1):
-            for i in range(qubits_number):
-                qc.ry( xs[i + qubits_number*j], i)
-            for i in range(qubits_number-1):
-                for k in range(i,qubits_number-1):
-                    qc.cz(i,k+1)
-        for i in range(qubits_number):
-            qc.ry(xs[i + qubits_number*(reps-1)], i)
-    return qc 
-
-'''
-VQT
-'''
 def sigmoid(x):
+    #aux = []
+    #for i in x:
+    #    aux.append( math.exp(i)/ (math.exp(i) + 1) )
+    #return np.array(aux)
     return np.exp(x) / (np.exp(x) + 1)
 
-def prob_function_ansatz(prob, qubits_number, bits_number):
-    qc = QuantumCircuit( qubits_number, bits_number)
-    for i in range(qubits_number):
-        #sigmoid_matrix = [[sigmoid(prob[i]), 0], [0, 1- sigmoid(prob[i])]]
-        #sigmoid_matrix = qi.Operator(sigmoid_matrix)
-        #qc.unitary(sigmoid_matrix, i)
-        qc.rz(prob[i], i)
-    qc.measure([i for i in range(qubits_number)], [i for i in range(qubits_number)])
-    return qc 
+def calculate_entropy(distribution):
+    total_entropy = 0
+    for d in distribution:
+        total_entropy += -1 * d[0] * np.log(d[0]) + -1 * d[1] * np.log(d[1])
+    return total_entropy
+    
+def number_rotation_params(rotation_set, qubits, reps):
+        return len(rotation_set)*qubits*reps
 
+def number_nonlocal_params(text, qubits, reps):
+    if text=='chain':
+        return int( (qubits-1)*reps )
+    elif text=='ring':
+        return int( qubits*reps )
+    elif text=='all_to_all':
+        return int( (qubits/2)*(qubits-1)*reps )
+    else:
+        return 0
