98204ed7bdde6d8b75a7cb39a099a5335683cfda
jnorambu <javinorambl@gmail.com>
2024-05-22 21:57:57 -0400

    Adding custom lattice to spin and FH model

--- quantumsim/lattice/lattice.py
+++ quantumsim/lattice/lattice.py
@@ -25,3 +25,28 @@ def lattice(params):
         lattice = nx.hexagonal_lattice_graph(params['size'][0], params['size'][1], periodicity)
 
     return lattice.edges(), lattice.nodes()
+
+"""
+Funcion para construir una grilla custom
+input:
+    params: diccionario con los parametros de la lattice
+        - node: numero de nodos en cada eje [X, Y]
+        - edges: lista de conecciones entre los nodos (n1, n2)
+return:
+    edges: lista con las conexiones entre los nodos
+    node: lista con los nodos del sistema
+"""
+def custom_lattice(params):
+    nodes = []
+    for i in range( params['node'][0] ):
+        nodes = nodes + [ (i,j) for j in range( params['node'][1] ) ]
+    
+    edges = []
+    for edge in params['edges']:
+        x1 = edge[0]
+        x2 = edge[1]
+        
+        t1 = ( x1%params['node'][0], x1//params['node'][0] )
+        t2 = ( x2%params['node'][0], x2//params['node'][0] )
+        edges.append( (t1, t2) )
+    return  edges, nodes
--- quantumsim/variational/vqe/spin.py
+++ quantumsim/variational/vqe/spin.py
@@ -1,5 +1,5 @@
 from .base import vqe_base
-from quantumsim.lattice import lattice
+from quantumsim.lattice import lattice, custom_lattice
 from pennylane import numpy as np
 import pennylane as qml
 
@@ -18,31 +18,31 @@ class vqe_spin(vqe_base):
         self.qubits = params['sites']
         terms = []
         coeff = []
-
-        if lat['lattice'] == 'custom':
-            pass
-        else:
-            x,y = lat['size']
+        
+        x,y = lat['size']
+        if lat['lattice'] != 'custom':
             lattice_edge, lattice_node = lattice(lat)
+        else:
+            lattice_edge, lattice_node = custom_lattice(lat)
+            
+        #Construir terminos asociados al exchange -J
+        for pair in lattice_edge:
+            termz = qml.PauliZ(wires=[ x*pair[0][0] + pair[0][1]])@qml.PauliZ(wires=[x*pair[1][0] + pair[1][1]])
+            termx = qml.PauliX(wires=[ x*pair[0][0] + pair[0][1]])@qml.PauliX(wires=[x*pair[1][0] + pair[1][1]])
+            termy = qml.PauliY(wires=[ x*pair[0][0] + pair[0][1]])@qml.PauliY(wires=[x*pair[1][0] + pair[1][1]])
 
-            #Construir terminos asociados al exchange -J
-            for pair in lattice_edge:
-                termz = qml.PauliZ(wires=[ x*pair[0][0] + pair[0][1]])@qml.PauliZ(wires=[x*pair[1][0] + pair[1][1]])
-                termx = qml.PauliX(wires=[ x*pair[0][0] + pair[0][1]])@qml.PauliX(wires=[x*pair[1][0] + pair[1][1]])
-                termy = qml.PauliY(wires=[ x*pair[0][0] + pair[0][1]])@qml.PauliY(wires=[x*pair[1][0] + pair[1][1]])
+            terms.extend( [termx, termy, termz] )
+            coeff.extend( [-params["J"][0]/4.0, -params["J"][1]/4.0, -params["J"][2]/4.0] )
+
+        #Construir terminos asociados al campo magnetico h
+        if 'h' in params:
+            for pair in lattice_node:
+                termz = qml.PauliZ(wires=[ x*pair[0] + pair[1]])
+                termx = qml.PauliX(wires=[ x*pair[0] + pair[1]])
+                termy = qml.PauliY(wires=[ x*pair[0] + pair[1]])
 
                 terms.extend( [termx, termy, termz] )
-                coeff.extend( [-params["J"][0]/4.0, -params["J"][1]/4.0, -params["J"][2]/4.0] )
-
-            #Construir terminos asociados al campo magnetico h
-            if 'h' in params:
-                for pair in lattice_node:
-                    termz = qml.PauliZ(wires=[ x*pair[0] + pair[1]])
-                    termz = qml.PauliX(wires=[ x*pair[0] + pair[1]])
-                    termz = qml.PauliY(wires=[ x*pair[0] + pair[1]])
-
-                    terms.extend( [termx, termy, termz] )
-                    coeff.extend( [params["h"][0]/2.0, params["h"][1]/2.0, params["h"][2]/2.0] )
+                coeff.extend( [params["h"][0]/2.0, params["h"][1]/2.0, params["h"][2]/2.0] )
         
         #Eliminar terminos cuyos coefficientes son 0
         to_delete = [i for i,c in enumerate(coeff) if np.abs(c)<1e-10 ]
